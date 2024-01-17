b9d6ef15c77d381fb69bf1fd63e337ab04ef63e3
adamlacue <117120196+adamlacue@users.noreply.github.com>
2024-01-16 19:22:18 -0500

    average of list done

--- averageOfList.js
+++ averageOfList.js
@@ -1,7 +1,10 @@
-const averageOfList = () => {
-    return(2)
+const averageOfList = (num1, num2, num3) => {
+    const nums = [num1, num2, num3];
     
+    const sum = nums.reduce((acc, num) => acc + num, 0);
+    const avg = sum / nums.length;
+    return avg;
 }
 
 
-module.exports = averageOfList
+module.exports = averageOfList;
--- averageOfList.test.js
+++ averageOfList.test.js
@@ -1,5 +1,5 @@
 const averageOfList = require("./averageOfList.js")
 
 test("average [1,2,3] ", () => {
-    expect(averageOfList([1,2,3])).toBe(2)
+    expect(averageOfList(1,2,3)).toBe(2)
 })
